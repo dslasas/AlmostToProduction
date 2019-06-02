dc94b3f1103f6140121eac5d33922238c3fd5cdc
dslasas <addison.dslasas@gmail.com>
2019-06-02 15:26:13 -0700

    Partially completed tutorial for CRUD transactions

---
+++ src/main/java/Controllers/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package Controllers;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
