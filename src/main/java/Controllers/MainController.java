36805c9e281c779431d1c3f1c270eec9efdeb888
dslasas <addison.dslasas@gmail.com>
2019-06-02 11:18:44 -0700

    Created demo objects for building endpoint

--- src/main/java/Controllers/MainController.java
+++ src/main/java/Controllers/MainController.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainController {
-
+    /* Todo : Complete 'Building REST services with Spring' and use it to create some simple REST operations */
+    /* Todo : How to map objects to database tables */
+    /* Todo : Connecting Spring application to a database */
+    /* Todo : Figure out how to deploy to AWS */
     public static void main(String[] args) {
         SpringApplication.run(MainController.class, args);
     }
