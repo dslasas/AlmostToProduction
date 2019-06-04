5d638f24d0c6ac435ec70c388b1c9d15dbbbfbd0
dslasas <addison.dslasas@gmail.com>
2019-06-04 07:54:21 -0700

    Added aggregate-root requests, and resource assembler

--- src/main/java/Controllers/EmployeeController.java
+++ src/main/java/Controllers/EmployeeController.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,20 +24,34 @@ public class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeResourceAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository,
+                       EmployeeResourceAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate Root
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    Resources<Resource<Employee>> all() {
+        List<Resource<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
-    Employee newEmployee(@RequestBody Employee newEmployee) {
-        return repository.save(newEmployee);
+    ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) throws URISyntaxException {
+
+        Resource<Employee> resource = assembler.toResource(repository.save(newEmployee));
+
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
     }
 
     // Single Item
@@ -42,10 +60,7 @@ public class EmployeeController {
         Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return new Resource<>(employee,
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
-
+        return assembler.toResource(employee);
     }
 
     @PutMapping("/employees/{id}")
